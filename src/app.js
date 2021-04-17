94e6526d68e3482e1d0c14b0be952eba74870e6a
Hong Shik Kim <hong@hongs.dev>
2021-04-17 12:03:04 +0900

    example: Use style-loader & css-loader for importing a css file in a js file & Update webpack & webpack-cli

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import * as math from './math.js';
 // import * as quiz from './quiz.js';
 // import * as callback from './async/callback.js';
 // import * as callback from './async/callback-to-promise.js';
-import * as async from './async/async.js';
+// import * as async from './async/async.js';
+import './app.css';
 
 console.log(math.sum(1, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,18 @@ module.exports = {
   mode: 'development',
   entry: {
     main: './src/app.js',
-    main1: './src/app.js'
+    main1: './src/app.js',
   },
   output: {
     path: path.resolve('./dist'),
     filename: '[name].js',
-  }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
